86957e2aebeab050853a70cead2acf521799c7c8
chenkeke <chenkeke@621171@qq.com>
2019-09-25 17:22:23 +0800

    添加【事件暂存类】【事件分发类】

---
+++ src/Foundation/EventGeneratorTrait.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+/**
+ *      Discuz & Tencent Cloud
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      1: EventGeneratorTrait.php 28830 2019-09-25 16:55 chenkeke $
+ */
+
+namespace Discuz\Foundation;
+
+trait EventGeneratorTrait
+{
+    /**
+     * 暂存将发生的事件
+     *
+     * @var array
+     */
+    protected $pendingEvents = [];
+
+    /**
+     * 添加新事件
+     *
+     * @param mixed $event
+     */
+    public function raise($event)
+    {
+        $this->pendingEvents[] = $event;
+    }
+
+    /**
+     * 获取并清空暂存的事件
+     *
+     * @return array
+     */
+    public function releaseEvents()
+    {
+        $events = $this->pendingEvents;
+        $this->pendingEvents = [];
+        return $events;
+    }
+}
