8fd60a84a4e345885daa9d0d0656bf54a4860161
leiyu <leiyu@comsenz-service.com>
2019-10-11 10:45:27 +0800

    file response

---
+++ src/Http/FileResponse.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Discuz\Http;
+
+
+use Illuminate\Http\File;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Zend\Diactoros\Response;
+
+class FileResponse extends Response {
+
+    protected $file;
+
+    public function __construct($file, int $status = 200, array $headers = [])
+    {
+        $this->setFile($file);
+        if (!isset($headers['Content-Type'])) {
+            $headers['Content-Type'] = $this->file->getMimeType() ?: 'application/octet-stream';
+        }
+        $body = $this->createBody();
+
+        parent::__construct($body, $status, $headers);
+    }
+
+    protected function createBody() {
+        return fopen($this->file->getRealPath(), 'rb');
+    }
+
+    /**
+     * Sets the file to stream.
+     *
+     * @param \SplFileInfo|string $file               The file to stream
+     * @param string              $contentDisposition
+     * @param bool                $autoEtag
+     * @param bool                $autoLastModified
+     *
+     * @return $this
+     *
+     * @throws FileException
+     */
+    public function setFile($file)
+    {
+        if (!$file instanceof \Symfony\Component\HttpFoundation\File\File) {
+            if ($file instanceof \SplFileInfo) {
+                $file = new File($file->getPathname());
+            } else {
+                $file = new File((string) $file);
+            }
+        }
+
+        if (!$file->isReadable()) {
+            throw new FileException('File must be readable.');
+        }
+
+        $this->file = $file;
+
+        return $this;
+    }
+}
