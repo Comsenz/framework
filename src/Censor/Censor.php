a544cc7073b21baf6743409f5163466e49969ea8
liudongdong <dde-mail@qq.com>
2019-10-18 17:10:17 +0800

    [add] stop word exception

--- src/Censor/Censor.php
+++ src/Censor/Censor.php
@@ -11,11 +11,12 @@ namespace Discuz\Censor;
 
 use App\Models\StopWord;
 use Discuz\Contracts\Setting\SettingRepository;
-use Illuminate\Support\LazyCollection;
 
 class Censor
 {
     public $config = 'default';
+
+    public $isMod = false;
 
     public $wordBanned = [];
 
@@ -33,37 +34,29 @@ class Censor
 
     public function check($content, $type = 'ugc') {
         // 设置关闭时，直接返回原内容
-        if ($this->isTurnOff) {
-            return $content;
-        }
+        // if ($this->isTurnOff) {
+        //     return $content;
+        // }
 
         StopWord::orderBy($type)->cursor()->tapEach(function ($word) use ($content, $type) {
-            $find = '/' . addcslashes($word, '/') . '/i';
+            $find = '/' . addcslashes($word->find, '/') . '/i';
             // dump($find);
             if ($word->{$type} == '{REPLACE}') {
-                if (preg_match_all($find, $content, $matches)) {
-                    dump($matches);
-                }
+                preg_replace($find, $word->replacement, $content);
             } else {
-                if (preg_match($find, $content, $matches)) {
-                    dump($matches);
+                if ($word->{$type} == '{MOD}') {
+                    if (preg_match($find, $content, $matches)) {
+                        $this->isMod = true;
+                    }
+                } else if ($word->{$type} == '{BANNED}') {
+                    if (preg_match($find, $content, $matches)) {
+                        throw new CensorNotPassedException('content_banned');
+                    }
                 }
-            }
-            if (preg_match_all($find, $content, $matches)) {
-                dump($matches);
-                // $this->words_found = $matches[0];
-                // $this->result = DISCUZ_CENSOR_BANNED;
-                // $this->words_found = array_unique($this->words_found);
-                // $message = $this->highlight($message, $banned_words);
-                // return DISCUZ_CENSOR_BANNED;
             }
         })->each(function ($word) {
             // 触发 tapEach
         });
-
-        // foreach ($stopWords as $word) {
-        //     echo $word->id;
-        // }
 
         return $content;
     }
