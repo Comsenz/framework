2c77cbe0e66c18a60d9c73d713da0f95912fd30e
liudongdong <dde-mail@qq.com>
2019-10-23 16:57:43 +0800

    [add] thread list

--- src/Api/Serializer/AbstractSerializer.php
+++ src/Api/Serializer/AbstractSerializer.php
@@ -2,8 +2,15 @@
 
 namespace Discuz\Api\Serializer;
 
+use Closure;
 use DateTime;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use InvalidArgumentException;
 use Tobscure\JsonApi\AbstractSerializer as BaseAbstractSerializer;
+use Tobscure\JsonApi\Collection;
+use Tobscure\JsonApi\Relationship;
+use Tobscure\JsonApi\Resource;
+use Tobscure\JsonApi\SerializerInterface;
 
 abstract class AbstractSerializer extends BaseAbstractSerializer
 {
@@ -38,5 +45,71 @@ abstract class AbstractSerializer extends BaseAbstractSerializer
         if ($date) {
             return $date->format(DateTime::RFC3339);
         }
+    }
+
+    /**
+     * Get a relationship builder for a has-one relationship.
+     *
+     * @param mixed $model
+     * @param string|Closure|SerializerInterface $serializer
+     * @param string $relation
+     * @return Relationship
+     * @throws BindingResolutionException
+     */
+    public function hasOne($model, $serializer, $relation)
+    {
+        if ($model->$relation) {
+            $serializer = $this->resolveSerializer($serializer, $model, $model->$relation);
+
+            $element = new Resource($model->$relation, $serializer);
+
+            return new Relationship($element);
+        }
+    }
+
+    /**
+     * Get a relationship builder for a has-many relationship.
+     *
+     * @param mixed $model
+     * @param string|Closure|SerializerInterface $serializer
+     * @param string $relation
+     * @return Relationship
+     * @throws BindingResolutionException
+     */
+    public function hasMany($model, $serializer, $relation)
+    {
+        if ($model->$relation) {
+            $serializer = $this->resolveSerializer($serializer, $model, $model->$relation);
+
+            $element = new Collection($model->$relation, $serializer);
+
+            return new Relationship($element);
+        }
+    }
+
+    /**
+     * @param mixed $serializer
+     * @param mixed $model
+     * @param mixed $data
+     * @return SerializerInterface
+     * @throws InvalidArgumentException
+     * @throws BindingResolutionException
+     */
+    protected function resolveSerializer($serializer, $model, $data)
+    {
+        if ($serializer instanceof Closure) {
+            $serializer = call_user_func($serializer, $model, $data);
+        }
+
+        if (is_string($serializer)) {
+            $serializer = app()->make($serializer);
+        }
+
+        if (! ($serializer instanceof SerializerInterface)) {
+            throw new InvalidArgumentException('Serializer must be an instance of '
+                .SerializerInterface::class);
+        }
+
+        return $serializer;
     }
 }
