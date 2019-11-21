9524283ff32db68dbde4070c92b2dae83ddf5381
yulei745 <yleimm@gmail.com>
2019-11-21 14:25:33 +0800

    Update .php-cs-fixer.sh

--- .github/workflows/.php-cs-fixer.sh
+++ .github/workflows/.php-cs-fixer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -z "$TOKEN" ]; then
-    echo -e "\e[0;31mGITHUB_TOKEN is not set."
+if [ -z "$PWD" ]; then
+    echo -e "\e[0;31mPWD is not set."
     exit 1
 fi
 
 git add src/* -f
 git commit -m "php-cs-fixer output for commit $GITHUB_SHA [Skip ci]"
 
-OUT="$(git push https://"$ACTOR":"$TOKEN"@github.com/"$GITHUB_REPOSITORY".git HEAD:"$GITHUB_REF" 2>&1 > /dev/null)"
+OUT="$(git push https://"$ACTOR":"$PWD"@github.com/"$GITHUB_REPOSITORY".git HEAD:"$GITHUB_REF" 2>&1 > /dev/null)"
 
 echo "$OUT"
