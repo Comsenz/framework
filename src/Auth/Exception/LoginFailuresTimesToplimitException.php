d18081772f47c3d6ef9099262399717e5ee8646f
xinghailong <122363686@qq.com>
2019-12-19 16:56:11 +0800

    修改异常信息

--- src/Auth/Exception/LoginFailuresTimesToplimitException.php
+++ src/Auth/Exception/LoginFailuresTimesToplimitException.php
@@ -12,7 +12,7 @@ use Throwable;
 
 class LoginFailuresTimesToplimitException extends Exception
 {
-    public function __construct($message = 'Login Times Failures Times Toplimit', $code = 401, Throwable $previous = null)
+    public function __construct($message = 'Login Failures Times Toplimit', $code = 401, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
