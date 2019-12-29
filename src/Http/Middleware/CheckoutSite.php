e19bf1487df1624413e3627a486037de44566792
ldd <dde-mail@qq.com>
2019-12-29 18:11:44 +0800

    [fixed] $user->groups -> $actor->groups

--- src/Http/Middleware/CheckoutSite.php
+++ src/Http/Middleware/CheckoutSite.php
@@ -9,6 +9,7 @@ namespace Discuz\Http\Middleware;
 
 use App\Models\Group;
 use Discuz\Auth\AssertPermissionTrait;
+use Discuz\Auth\Exception\PermissionDeniedException;
 use Discuz\Contracts\Setting\SettingsRepository;
 use Discuz\Foundation\Application;
 use Illuminate\Support\Carbon;
@@ -22,6 +23,7 @@ class CheckoutSite implements MiddlewareInterface
     use AssertPermissionTrait;
 
     protected $app;
+
     protected $settings;
 
     public function __construct(Application $app, SettingsRepository $settings)
@@ -34,7 +36,7 @@ class CheckoutSite implements MiddlewareInterface
      * @param ServerRequestInterface $request
      * @param RequestHandlerInterface $handler
      * @return ResponseInterface
-     * @throws \Discuz\Auth\Exception\PermissionDeniedException
+     * @throws PermissionDeniedException
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
@@ -48,10 +50,11 @@ class CheckoutSite implements MiddlewareInterface
         $actor = $request->getAttribute('actor');
         $siteClose && $this->assertAdmin($actor);
 
-        //处理 付费模式 逻辑， 过期之后 加入待付费组
-        if(!$actor->isAdmin() && $siteMode === 'pay' && Carbon::now()->gt($actor->expired_at)) {
-            $actor->groups = Group::where('id',Group::UNPAID)->get();
+        // 处理 付费模式 逻辑， 过期之后 加入待付费组
+        if (! $actor->isAdmin() && $siteMode === 'pay' && Carbon::now()->gt($actor->expired_at)) {
+            $actor->groups = Group::where('id', Group::UNPAID)->get();
         }
+
         return $handler->handle($request);
     }
 }
