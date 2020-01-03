01b7815f68663da3b3dbf88c7b68e8e098559be8
Harvey <619596123@qq.com>
2020-01-03 11:02:09 +0800

    添加语言包异常

---
+++ src/Locale/AbstractLocaleException.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Discuz\Locale;
+
+use Exception;
+
+/**
+ * Class AbstractLocaleException
+ *
+ * @package Discuz\Locale
+ */
+abstract class AbstractLocaleException extends Exception
+{
+    /**
+     * 附加错误组
+     *
+     * @var array
+     */
+    protected $detail = [];
+
+    protected $message;
+
+    public function __construct($message = '', $code = 500, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * 错误数组
+     *
+     * @return array
+     */
+    abstract protected function getDetail() : array ;
+
+    /**
+     * 获取文件名
+     *
+     * @return mixed
+     */
+    public function getLocaleName()
+    {
+        $arr = explode('_', $this->message);
+        return array_shift($arr);
+    }
+}
