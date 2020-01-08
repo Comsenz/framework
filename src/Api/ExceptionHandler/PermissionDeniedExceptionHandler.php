92233a9229985bac6228d0da63b707f14a7d59c6
xinghailong <122363686@qq.com>
2020-01-08 14:05:08 +0800

    20200108 增加注册审核设置；修改导出用户映射；修改禁用用户拦截；

--- src/Api/ExceptionHandler/PermissionDeniedExceptionHandler.php
+++ src/Api/ExceptionHandler/PermissionDeniedExceptionHandler.php
@@ -36,12 +36,13 @@ class PermissionDeniedExceptionHandler implements ExceptionHandlerInterface
 
         // 站点是否关闭
         $settings = app()->make(SettingsRepository::class);
-
         if ($settings->get('site_close')) {
             $error['code'] = 'site_closed';
             $error['detail'] = $settings->get('site_close_msg');
         } elseif ($e->getMessage() == 'ban_user') {
             $error['code'] = 'ban_user';
+        } elseif ($e->getMessage() == 'register_validate') {
+            $error['code'] = 'register_validate';
         } else {
             $error['code'] = 'permission_denied';
         }
