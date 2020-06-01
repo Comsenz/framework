e5855f1ad0f40dee0322db283ca61a630dbb88c6
xinghailong <122363686@qq.com>
2020-06-01 16:40:42 +0800

    refactor(attachment): 修改附件的数据结构和类型支持扩展

--- src/Foundation/AbstractUploadTool.php
+++ src/Foundation/AbstractUploadTool.php
@@ -90,102 +90,6 @@ abstract class AbstractUploadTool implements UploadTool
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function upload(UploadedFileInterface $file, $uploadPath = '', $uploadName = '', $options = [])
-    {
-        $this->file = $file;
-
-        $uploadPath = $uploadPath . date('/Y/m/d');
-
-        $this->extension = Str::lower(pathinfo($this->file->getClientFilename(), PATHINFO_EXTENSION));
-
-        $this->uploadPath = $uploadPath ?: $this->uploadPath;
-
-        $fileName = Str::random();
-
-        $this->uploadName = $uploadName ?: $fileName . '.' . $this->extension;
-
-        $this->options = is_string($options)
-            ? ['visibility' => $options]
-            : ($options ?: $this->options);
-
-        /**
-         * @see 云上数据处理 https://cloud.tencent.com/document/product/460/18147#.E4.BA.91.E4.B8.8A.E6.95.B0.E6.8D.AE.E5.A4.84.E7.90.86
-         */
-        if ($this->file->isGallery && $this->getIsRemote()) {
-            $this->options = array_merge($this->options, [
-                'header' => [
-                    'PicOperations' => json_encode([
-                        'rules' => [
-                            [
-                                'fileid' => md5($fileName) . '_blur.' . $this->extension,
-                                'rule' => 'imageMogr2/thumbnail/500x500/blur/35x15',
-                            ]
-                        ],
-                    ]),
-                ]
-            ]);
-        }
-
-        $this->fullPath = trim($this->uploadPath . '/' . $this->uploadName, '/');
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @param array $type
-     * @param int $size
-     * @return bool|array
-     * @throws UploadVerifyException
-     * @throws FileExistsException
-     */
-    public function save(array $type = [], int $size = 0)
-    {
-        $this->verifyFileType($type);
-
-        $this->verifyFileSize($size);
-
-        if ($this->error) {
-            throw new UploadVerifyException();
-        }
-
-        $stream = $this->file->getStream();
-
-        if ($this->file->getSize() > 10 * 1024 * 1024) {
-            $resource = $stream->detach();
-            $result = $this->filesystem->writeStream($this->fullPath, $resource, $this->options);
-
-            if (is_resource($resource)) {
-                fclose($resource);
-            }
-        } else {
-            $result = $this->filesystem->put($this->fullPath, $stream->getContents(), $this->options);
-
-            $stream->close();
-        }
-
-        $isRemote = [
-            'isRemote' => $this->getIsRemote()
-        ];
-        if ($this->getIsRemote()) {
-            $fileInfo = [
-                'url' =>  $this->filesystem->getAdapter()->getSourcePath($this->fullPath),
-                'path' => $this->fullPath
-            ];
-        } else {
-            $fileInfo = [
-                'url' => $this->filesystem->url($this->fullPath),
-                'path' => $this->filesystem->path($this->fullPath)
-            ];
-        }
-        return $result ? Arr::collapse([$isRemote, $fileInfo]) : false;
-
-    }
-
-    /**
      * 删除附件
      *
      * @param Attachment $attachment
@@ -200,7 +104,7 @@ abstract class AbstractUploadTool implements UploadTool
         $result = $this->contractsFilesystem->disk($remote ? 'attachment_cos' : 'attachment')->delete($path);
 
         // 如果是帖子图片,删除本地缩略图
-        if ($attachment->is_gallery) {
+        if ($attachment->type == Attachment::TYPE_OF_IMAGE) {
             $thumb = $attachment::replaceThumb($path);
             $this->contractsFilesystem->disk('attachment')->delete($thumb);
         }
